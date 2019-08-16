da94d5fc493dc5514cfdae717af81ef2b44123dd
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-08-16 14:39:14 +0300

    remove dotenv

--- src/controller/get-books.js
+++ src/controller/get-books.js
@@ -1,12 +1,9 @@
 const nodeFetch = require('node-fetch');
 const { render } = require('./../views/helpers');
 
-require('dotenv').config();
-
-const url = process.env.URL;
 exports.getBooks = (req, res, next) => {
   const query = encodeURIComponent(req.query['book-name']);
-  nodeFetch(`${url}${query}`)
+  nodeFetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
     .then((res1) => res1.json())
     .then((res2) => render(res2.items))
     .then((imageInfo) => res.render('home', { imageInfo }))
